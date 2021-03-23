9ab584e8ee3528a49d18d89f9df407a60ba37cc6
kadweka <husseinkatana17@gmail.com>
2021-03-24 02:30:06 +0300

    changed the app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ bootstrap = Bootstrap()
 def create_app(config_name):
     '''
     Function that takes configuration setting key as an argument
-    
+
     Args:
         config_name : name of the configuration to be used
     '''
@@ -26,7 +26,7 @@ def create_app(config_name):
     app = Flask(__name__)
 
     # Creating the app configurations
-    app.config.from_object(config_options[config_name])
+    # app.config.from_object(config_options[config_name])
 
 
     # Initialising flask extensions
