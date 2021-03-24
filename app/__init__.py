5e3298e3811df9950c01c39294737605724c89a8
kadweka <husseinkatana17@gmail.com>
2021-03-24 15:22:02 +0300

    connect app to database

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,7 @@ def create_app(config_name):
     app = Flask(__name__)
 
     # Creating the app configurations
-    app.config.from_object(config_options[config_name])
+    app.config.from_object(config_options['development'])
 
 
     # Initialising flask extensions
