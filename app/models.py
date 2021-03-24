721c3d34b08e2af8d4cd5b08fb783343c977bc12
kadweka <husseinkatana17@gmail.com>
2021-03-24 03:14:59 +0300

    connected ap to database

--- app/models.py
+++ app/models.py
@@ -1,1 +1,9 @@
 # File for models/classes
+from . import db
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
--- config.py
+++ config.py
@@ -1,42 +1,41 @@
 import os
 
 class Config:
-    '''
-    General configuration parent class
-    '''
-    pass
+   '''
+   General configuration parent class
+   '''
 
+   # simple mde  configurations
+   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+   DATABASE_URL ='postgresql+psycopg2://kadweka:qwerty12@localhost/ma3'
+   SECRET_KEY = os.environ.get('SECRET_KEY')
+   SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+# export DATABASE_URL='postgresql+psycopg2://kadweka:qwerty12@localhost/ma3'
+
+# python3.6 manage.py server
 class ProdConfig(Config):
-    '''
-    Pruduction configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
+   '''
+   Production  configuration child class
+   Args:
+       Config: The parent configuration class with General configuration settings
+   '''
+   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+   DATABASE_URI ='postgresql+psycopg2://kadweka:qwerty12@localhost/ma3'
 
 class TestConfig(Config):
-    '''
-    Testing configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings 
-    '''
-    pass
-
+    DATABASE_URI ='postgresql+psycopg2://kadweka:qwerty12@localhost/ma3'
 class DevConfig(Config):
-    '''
-    Development configuration child class
-    
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
-    DEBUG = True
-
+   '''
+   Development  configuration child class
+   Args:
+       Config: The parent configuration class with General configuration settings
+   '''
+   SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:qwerty12@localhost/ktown003'
+   DEBUG = True
 
 config_options = {
-    'development': DevConfig,
-    'production': ProdConfig,
-    'test': TestConfig
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
 }
