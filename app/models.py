76b5131d50cec9336ebc28383ca30d6735d30b8b
kadweka <husseinkatana17@gmail.com>
2021-04-01 20:58:11 +0300

    add more fields for the company class

--- app/models.py
+++ app/models.py
@@ -3,5 +3,7 @@ from . import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     compamy_email = db.Column(db.String(100), unique=True)
+    mobile=db.Column(db.Integer())
     password = db.Column(db.String(100))
     company_name = db.Column(db.String(1000))
+    company_website=db.Column(db.String())
